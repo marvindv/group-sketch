420ab75dd35a37532e6c9374bac90f29ea28a37f
Marvin Davieds <marvin.davieds@gmail.com>
2020-04-16 09:20:47 +0200

    feat(app): remove custom font stack
    ... since it looked ugly on windows.

--- packages/app/src/App.vue
+++ packages/app/src/App.vue
@@ -6,9 +6,6 @@
 
 <style lang="scss">
 #app {
-  // From https://gist.github.com/don1138/5761014
-  font-family: Impact, Haettenschweiler, "Franklin Gothic Bold", Charcoal,
-    "Helvetica Inserat", "Bitstream Vera Sans Bold", "Arial Black", sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
 }
